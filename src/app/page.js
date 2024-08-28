d8e683bfc950730f24a94a63ff4dea5db61cbdf3
Anas-Styleloom <hussainanas68@gmail.com>
2024-08-28 13:25:00 +0500

    portfolio

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="text-4xl flex justify-center items-center mt-80">
+      Hey! I'm Anas
+    </div>
+  )
+}
+
+export default page
