b90ab086e76cbdc80bdb9d65122a676b070ed09a
anas hussain <hussainanas68@gmail.com>
2025-03-31 21:48:43 +0500

    added

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,8 +6,34 @@ import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Anas Hussain",
-  description: "I am Frontend Next JS developer",
+  title: "Anas Hussain | Frontend Web Developer",
+  description:
+    "A Frontend Developer specializes in building visually appealing, user-friendly, and high-performance websites using modern web technologies. They focus on creating responsive designs, optimizing website speed, and improving user experience to enhance SEO (Search Engine Optimization) rankings.",
+  keywords:
+    "Frontend Developer, SEO Optimization, web development, Responsive Design, Mobile-Friendly Websites,User Interface (UI) Design,JavaScript Developer,React Developer,Next.js Developer,Web Performance Optimization,,Core Web Vitals,SEO Best Practices,HTML/CSS,Cross-Browser Compatibility,Page Load Speed,Search Engine Visibility,Structured Data,Meta Tags Implementation,JavaScript Frameworks,Accessibility Improvements,Front-End Development",
+  authors: [{ name: "Anas Hussain", url: "https://anashussain.com" }],
+  openGraph: {
+    title: "Anas Hussain | Frontend Web Developer",
+    description:
+      "A Frontend Developer specializes in building visually appealing, user-friendly, and high-performance websites using modern web technologies. They focus on creating responsive designs, optimizing website speed, and improving user experience to enhance SEO (Search Engine Optimization) rankings.",
+    url: "https://anashussain.com",
+    siteName: "Anashussain",
+    images: [
+      {
+        url: "https://anashussain.com/images/aitoolcity.png",
+        width: 1200,
+        height: 630,
+        alt: "anashussain",
+      },  
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Anas Hussain | Frontend Web Developer",
+    description: "A Frontend Developer specializes in building visually appealing, user-friendly, and high-performance websites using modern web technologies. They focus on creating responsive designs, optimizing website speed, and improving user experience to enhance SEO (Search Engine Optimization) rankings.",
+    images: ["https://anashussain.com/images/aitoolcity.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
