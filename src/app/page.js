d5f9d1b058f3653d5e0b5868f8c990b4d3b2fd1d
Anas Hussain <hussainanas68@gmail.com>
2025-08-06 23:34:36 +0500

    refreshed

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 import React from "react";
-import About from "./components/About.jsx";
-import Projects from "./components/Projects.jsx";
-import Hero from "./components/Hero.jsx";
-import Contact from "./components/Contact.jsx";
-import Header from "./components/Header.jsx";
+import About from "./components/About.js";
+import Projects from "./components/Projects.js";
+import Hero from "./components/Hero.js";
+import Contact from "./components/Contact.js";
+import Header from "./components/Header.js";
 
 const page = () => {
   return (
